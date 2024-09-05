9fa038deeb374e40ebad4d44bd521828a279664f
Jahleel A <jahleelabraham@hotmail.com>
2024-09-05 16:02:14 +0000

    Fix for Device's failing to delete

--- src/routes/controllers/Devices/DeleteDevice.ts
+++ src/routes/controllers/Devices/DeleteDevice.ts
@@ -11,10 +11,80 @@ export async function deleteDevice(
 ): Promise<void> {
   let deviceId = req.params.deviceId ?? -1;
 
-  Devices.findOne({where: {id: deviceId}})
-    .then(device => {
-      let id = device.dataValues.id || -1;
-      if (id == -1) {
+  let attempts = 10;
+  let shouldRetry = false;
+
+  //FIXME: Remove this retry loop. This same code is present in PHP due to the posibility of new Media being created while the device is being deleted.
+  do {
+    attempts--;
+    Devices.findOne({where: {id: deviceId}})
+      .then(device => {
+        let id = device.dataValues.id || -1;
+        if (id == -1) {
+          res
+            .status(404)
+            .send(
+              new ErrorResponse(
+                404,
+                `A device with the ID ${deviceId} was not found!`
+              )
+            );
+          return;
+        }
+        try {
+          Server.sequelize.query(
+            `DELETE FROM EventsCam WHERE device_id='${id}'`
+          );
+          Server.sequelize.query(
+            `DELETE FROM OnvifEvents WHERE device_id='${id}'`
+          );
+          Server.sequelize
+            .query(
+              `SELECT id, filepath FROM Media WHERE device_id='${id}' ORDER BY id DESC`
+            )
+            .then((result: [any, any]) => {
+              if (result[0].length > 0) {
+                let highestId = result[0][0].id;
+                result[0].forEach((media: any) => {
+                  fs.unlink(media.filepath, err => {
+                    if (err) {
+                      Server.Logs.error(
+                        `An error occured when deleting media belonging to ID ${deviceId} - ${err}`
+                      );
+                    }
+                  });
+                });
+                try {
+                  Server.sequelize.query(
+                    `DELETE FROM Media WHERE device_id='${id}' AND id <= ${highestId}`
+                  );
+                } catch (err) {
+                  shouldRetry = true;
+                }
+              }
+            });
+          device.destroy();
+          res
+            .status(200)
+            .send(new ErrorResponse(200, 'Deleted device sucessfully!'));
+          return;
+        } catch (err) {
+          shouldRetry = true;
+          Server.Logs.error(
+            `An error occured when deleting device of ID ${deviceId} - ${err}`
+          );
+          res
+            .status(500)
+            .send(
+              new ErrorResponse(
+                500,
+                `An error occured when deleting device of ID ${deviceId}! Please report this to the Bluecherry team along with logs`
+              )
+            );
+          return;
+        }
+      })
+      .catch(err => {
         res
           .status(404)
           .send(
@@ -24,62 +94,17 @@ export async function deleteDevice(
             )
           );
         return;
-      }
-      try {
-        Server.sequelize.query(`DELETE FROM EventsCam WHERE device_id='${id}'`);
-        Server.sequelize.query(
-          `DELETE FROM OnvifEvents WHERE device_id='${id}'`
-        );
-        Server.sequelize
-          .query(
-            `SELECT id, filepath FROM Media WHERE device_id='${id}' ORDER BY id DESC`
-          )
-          .then((result: [any, any]) => {
-            if (result[0].length > 0) {
-              let highestId = result[0][0].id;
-              result[0].forEach((media: any) => {
-                fs.unlink(media.filepath, err => {
-                  if (err) {
-                    Server.Logs.error(
-                      `An error occured when deleting media belonging to ID ${deviceId} - ${err}`
-                    );
-                  }
-                });
-              });
-              Server.sequelize.query(
-                `DELETE FROM Media WHERE device_id='${id}' AND id <= ${highestId}`
-              );
-            }
-          });
-        device.destroy();
-        res
-          .status(200)
-          .send(new ErrorResponse(200, 'Deleted device sucessfully!'));
-        return;
-      } catch (err) {
-        Server.Logs.error(
-          `An error occured when deleting device of ID ${deviceId} - ${err}`
-        );
-        res
-          .status(500)
-          .send(
-            new ErrorResponse(
-              500,
-              `An error occured when deleting device of ID ${deviceId}! Please report this to the Bluecherry team along with logs`
-            )
-          );
-        return;
-      }
-    })
-    .catch(err => {
-      res
-        .status(404)
-        .send(
-          new ErrorResponse(
-            404,
-            `A device with the ID ${deviceId} was not found!`
-          )
-        );
-      return;
-    });
+      });
+  } while (shouldRetry && attempts > 0);
+
+  if (attempts == 0 && shouldRetry) {
+    res
+      .status(500)
+      .send(
+        new ErrorResponse(
+          500,
+          `Failed to deleted the device of ID ${deviceId}! Please report this to the Bluecherry team along with logs`
+        )
+      );
+  }
 }
