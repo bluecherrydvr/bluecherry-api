728d9afe4f7de1cefcd550417a656f2b980acb31
Curtis Hall <curt@bluecherry.net>
2025-03-08 23:10:58 -0600

    Add routes for tour and absolute movement

--- src/routes/controllers/Devices/DevicesController.ts
+++ src/routes/controllers/Devices/DevicesController.ts
@@ -25,11 +25,16 @@ api.route('/:deviceId').put(updateDevice);
 // PTZ routes
 api.route('/:deviceId/ptz/continuous').post(ptzContinuous);
 api.route('/:deviceId/ptz/absolute').post(ptzAbsolute); 
+api.route('/:deviceId/ptz/relative').post(ptzRelative);
 api.route('/:deviceId/ptz/stop').post(ptzStop);
 
 // PTZ preset routes
 api.route('/:deviceId/ptz/presets').get(getPtzPresets);
 api.route('/:deviceId/ptz/preset').post(gotoPreset);
 api.route('/:deviceId/ptz/preset/set').post(setPreset);
+
+// Pattern/Tour routes
+api.route('/:deviceId/ptz/pattern/record').post(recordPattern);
+api.route('/:deviceId/ptz/pattern/run').post(runPattern);
 
 export = api;
