6d16ab68bf67bbfaa550442f4b1db4bf9aaaf356
Curtis Hall <curt@bluecherry.net>
2025-03-08 22:47:20 -0600

    Update DevicesController.ts

--- src/routes/controllers/Devices/DevicesController.ts
+++ src/routes/controllers/Devices/DevicesController.ts
@@ -4,6 +4,14 @@ import {deleteDevice} from './DeleteDevice';
 import {getDevice, getDevices} from './GetDevice';
 import {updateDevice} from './UpdateDevice';
 import {ptzContinuous, ptzAbsolute, ptzStop} from './PtzDevice';
+import {
+    ptzContinuous, 
+    ptzAbsolute, 
+    ptzStop,
+    getPtzPresets,
+    gotoPreset,
+    setPreset
+} from './PtzDevice';
 
 
 const api = Router();
@@ -18,5 +26,10 @@ api.route('/:deviceId').put(updateDevice);
 api.route('/:deviceId/ptz/continuous').post(ptzContinuous);
 api.route('/:deviceId/ptz/absolute').post(ptzAbsolute); 
 api.route('/:deviceId/ptz/stop').post(ptzStop);
+
+// PTZ preset routes
+api.route('/:deviceId/ptz/presets').get(getPtzPresets);
+api.route('/:deviceId/ptz/preset').post(gotoPreset);
+api.route('/:deviceId/ptz/preset/set').post(setPreset);
 
 export = api;
