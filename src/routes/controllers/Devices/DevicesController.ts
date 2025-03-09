24101b270a3f6e55ef1901256133ed76caf160f2
Curtis Hall <curt@bluecherry.net>
2025-03-08 22:43:22 -0600

    Update DevicesController.ts

--- src/routes/controllers/Devices/DevicesController.ts
+++ src/routes/controllers/Devices/DevicesController.ts
@@ -3,6 +3,8 @@ import {addDevice} from './AddDevice';
 import {deleteDevice} from './DeleteDevice';
 import {getDevice, getDevices} from './GetDevice';
 import {updateDevice} from './UpdateDevice';
+import {ptzContinuous, ptzAbsolute, ptzStop} from './PtzDevice';
+
 
 const api = Router();
 
@@ -11,5 +13,10 @@ api.route('/:format').get(getDevices);
 api.route('/:deviceId/:format').get(getDevice);
 api.route('/:deviceId').delete(deleteDevice);
 api.route('/:deviceId').put(updateDevice);
+
+// PTZ routes
+api.route('/:deviceId/ptz/continuous').post(ptzContinuous);
+api.route('/:deviceId/ptz/absolute').post(ptzAbsolute); 
+api.route('/:deviceId/ptz/stop').post(ptzStop);
 
 export = api;
